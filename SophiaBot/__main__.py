1f4ac21be77ad073181d33cc021efc52e407ddca
grue47553 <92097462+grue47553@users.noreply.github.com>
2021-10-16 13:52:05 +0530

    Update __main__.py

--- SophiaBot/__main__.py
+++ SophiaBot/__main__.py
@@ -45,7 +45,8 @@ from telegram.ext import (
     Filters,
     MessageHandler,
 )
-from telegram.ext.dispatcher import DispatcherHandlerStop,UTR4E""" m telegram.utils.helpers import escape_markdown
+from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
+from telegram.utils.helpers import escape_markdown
 
 
 def get_readable_time(seconds: int) -> str:
@@ -73,36 +74,20 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = """Hey there 👋! My name is *ɴᴀʀᴜᴛᴏ*.\n\nI can help manage your groups with useful features, feel free to add me to your groups!."""
+PM_START_TEXT = """Hey there 👋! My name is *Vixen*.\n\nI can help manage your groups with useful features, feel free to add me to your groups!."""
 
 STICKERS = (
-      "CAACAgUAAx0CWrNeBQAC8xxhaWWJYna16TZXVcOdvPS9InnhfgACrAMAAn37MVV0qkjG9l_UTR4E",
-)  
+      "CAACAgUAAx0CTpRfGwACF41hMfagTfWdHNFici1VtOCQVVNYmgACgh8AAsZRxhU6tKJa_ySnnCAE",
+      "CAACAgUAAx0CTpRfGwACF5phMfi5vgKwQFg6KuzHiEc79QFT0QACCR4AAsZRxhVu32VqEb3_1SAE",
+      "CAACAgUAAx0CTpRfGwACGIBhQcGJDHuuXsU5el3I86SEx3nTpgAC2R8AAsZRxhUpEe6EcVukQCAE",
+)    
 
 
-buttons = [
-    [
-        InlineKeyboardButton(text=" Commands Help ❓", callback_data="help_back"),
-    ],
-    [
-        InlineKeyboardButton(text="Info & About 🙋‍", callback_data="source_"),
-        InlineKeyboardButton(
-                  text="System Stats 💻", callback_data="stats_callback"
-        ),
-    ],
-    [
-        InlineKeyboardButton(text="NARUTO NEWS 🙋‍♂️ ", url=f"https://t.me/SophiaUpdates"),
-        InlineKeyboardButton(text=" Support Group 💬 ", url=f"https://t.me/SophiaSupport_Official"),
-    ],
-    [
-        InlineKeyboardButton(text="➕ Add ɴᴀʀᴜᴛᴏ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ  to your Group ➕", url="t.me/SophiaSLBot?startgroup=true"),   
-    ],
-]
 
 HELP_STRINGS = """
 ✘✘✘ 𝗛𝗲𝗹𝗽𝗳𝘂𝗹 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀 ✘✘✘
 
-Every possibility of Sophia is documentated here
+Every possibility of Vixen is documentated here
 Click buttons to get help
 """
 
@@ -219,7 +204,7 @@ def start(update: Update, context: CallbackContext):
                 timeout=60,
             )
     else:
-        update.effective_message.reply_text("Heya, Sophia here :) PM me if you have any questions how to use me!")
+        update.effective_message.reply_text("Heya, Vixen here : )PM me if you have any questions how to use me!")
 
 
 def error_handler(update, context):
